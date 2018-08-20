e37d416477b4cca1b27692999b71a9f47a5d2db9
Addison Cole <addison.cole85@gmail.com>
2018-08-20 13:54:11 -0700

    completed word count method, runs all specs

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,13 @@
 
+def word_count(words)
+  words_arr = words.split(' ')
+  word_count_hash = {}
+  words_arr.each do |word|
+    if word_count_hash.has_key?(word)
+      word_count_hash[word] += 1
+    else
+      word_count_hash.merge!(word => 1)
+    end
+  end
+  return word_count_hash
+end
