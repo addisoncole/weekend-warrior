3efd54959ecd1c5c37639f86b0a3308f9ef41f6b
Addison Cole <addison.cole85@gmail.com>
2018-08-20 19:12:49 -0700

    add basic method meeting basic requirements, no specs

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,12 @@
-
+def robot_name ()
+  robot_name = String.new
+  letter_set = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+  2.times do
+    random = rand(0..25)
+    robot_name += letter_set[random]
+  end
+  3.times do
+    robot_name += rand(0..9).to_s
+  end
+  return robot_name
+end
