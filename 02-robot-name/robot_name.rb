6ba9c919cb09a7bdc3415650d4c61147c09b4dcc
Addison Cole <addison.cole85@gmail.com>
2018-08-26 16:24:46 -0700

    create Robot class

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,12 +1,47 @@
-def robot_name ()
-  robot_name = String.new
-  letter_set = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
-  2.times do
-    random = rand(0..25)
-    robot_name += letter_set[random]
+class Robot
+
+attr_accessor :name
+
+  def initialize
+    robot_name = String.new
+    letter_set = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+    2.times do
+      random = rand(0..25)
+      robot_name += letter_set[random]
+    end
+    3.times do
+      robot_name += rand(0..9).to_s
+    end
+    @name = robot_name
   end
-  3.times do
-    robot_name += rand(0..9).to_s
+
+  def reset
+    robot_name = String.new
+    letter_set = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+    2.times do
+      random = rand(0..25)
+      robot_name += letter_set[random]
+    end
+    3.times do
+      robot_name += rand(0..9).to_s
+    end
+    @name = robot_name
   end
-  return robot_name
 end
+
+# lassie = Dog.new
+# lassie.name = "Lassie"
+#
+# puts lassie.name
+# def robot_name ()
+#   robot_name = String.new
+#   letter_set = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+#   2.times do
+#     random = rand(0..25)
+#     robot_name += letter_set[random]
+#   end
+#   3.times do
+#     robot_name += rand(0..9).to_s
+#   end
+#   return robot_name
+# end
